81071f5eca57ad94f694706624a115fe9977842b
Lootere <looterakrish@gmail.com>
2025-01-06 09:40:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,9 +246,8 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : [ @RKMOVIESZIP ] <a href="https://t.me/Rkmovieszip">{file_name}</a> 
-
-
+    CAPTION = """<b> [ @RKMOVIESZIP ] <a href="https://t.me/Rkmovieszip">{file_name}</a>
+    
 <blockquote>✅ 𝘼𝙡𝙨𝙤 𝙟𝙤𝙞𝙣 #𝙍𝙠𝙢𝙤𝙫𝙞𝙚𝙯𝙞𝙥 – #𝙉𝙤𝟏 𝙏𝙚𝙡𝙚𝙜𝙧𝙖𝙢 𝙁𝙧𝙚𝙚 𝙈𝙤𝙫𝙞𝙚𝙨 𝘼𝙣𝙙 𝙒𝙚𝙗 𝙎𝙚𝙧𝙞𝙚𝙨 𝙀𝙣𝙩𝙚𝙧𝙩𝙖𝙞𝙣𝙢𝙚𝙣𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡. 
 
 ⏤‌‌𝗝⌡𝗼𝗶𝗻 𝗙𝗿𝗲𝗲 –「 @RKMOVIESZIP 」🔥</blockquote>
