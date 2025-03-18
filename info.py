011444506a8d36b84159aac9ac306dfd72acedc5
Lootere <looterakrish@gmail.com>
2025-03-18 14:01:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -161,7 +161,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://content-marianna-rohanworld-0c9c115e.koyeb.app/")
+URL = environ.get("URL", "aesthetic-brigitta-ramufastfood-1d95f97c.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
