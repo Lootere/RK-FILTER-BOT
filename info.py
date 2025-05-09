92a8f889ee585a35cc4adb3b3aa8d7488287b330
Lootere <looterakrish@gmail.com>
2025-05-09 10:29:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -165,7 +165,7 @@ URL = environ.get("URL", "aesthetic-brigitta-ramufastfood-1d95f97c.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
-RENAME_MODE = bool(environ.get('RENAME_MODE', False)) # Set True or False
+RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or False
 
 
 # Auto Approve Info : If True Then Bot Approve New Upcoming Join Request Else Not
